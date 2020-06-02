bf80f0674e2027c61852a731ac0257fd17590dba
Zane Finner <zanefinnergm@gmail.com>
2020-06-02 13:20:26 -0400

    dir structrure graphed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # dashboard-app
+
+## Architecture Run-Down
+
+### Main PHP Codebase
+This will be the code which sends pages, retrieves data, and stores sessions
+It will reach out to the microservices that are needed at a given time
+
+### GO Microservices
+These will handle all the modular things like sign-ins and using the ebay api
+
+## Run the codebases
+
+To run the codebases, treat each directory like a separate server. CD into go, and run main.go on port 3000. Then, go to the php directory, and run this on port 8000.
+
+To load the webpages, use :8000. Every microservice will run under 3000 so don't create any conflicts with the port numbers
---
+++ app/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+</head>
+<body>
+    <div class="nav" style="background-color: rgb(40,40,40); color: white;">
+        <div class="container">
+            <a href="/" style="color: white; text-decoration: none; font-size: 30px; line-height: 43px;">Home</a>
+        </div>
+    </div>
+    <div class="jumbotron">
+        <div class="container">
+            <h2>Tagline number 1</h2>
+            <p>Londer tagline number 2 here haha</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h2>Secure</h2>
+                <p>Exercitation ad occaecat aute voluptate consequat Lorem commodo qui velit amet esse ad ut nisi. Incididunt nulla veniam ut occaecat sit. Minim Lorem laboris sit id fugiat eu elit officia labore excepteur eu nulla officia mollit.</p>
+            </div>
+            <div class="col-12 col-md-6">
+                <h2>Efficient</h2>
+                <p>Tempor qui ad nulla laborum Lorem aliquip. Ullamco excepteur esse velit ullamco sit. Quis laborum fugiat est ullamco dolore laborum. Sint sit est amet deserunt officia Lorem tempor eiusmod do Lorem consequat et quis.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
